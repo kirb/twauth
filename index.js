e576944f959182d37a54f460060cc0b4dfc9116b
Adam D <kirbylover4000@gmail.com>
2015-03-22 18:41:34 +1030

    clean up colors a bit

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ function getAccessToken(oa, oauth_token, oauth_token_secret, pin) {
 					throw new Error('The pin number you have entered is incorrect'.bold.red);
 				}
 			}
-			console.log('Your OAuth Access Token: '.green + (oauth_access_token).bold.cyan);
-			console.log('Your OAuth Token Secret: '.green + (oauth_access_token_secret).bold.cyan);
+			console.log('Your OAuth Access Token: '.bold.green + oauth_access_token);
+			console.log('Your OAuth Token Secret: '.bold.green + oauth_access_token_secret);
 			console.log('Now, save these two values, along with your origional consumer secret and key and use these in your twitter app'.bold.yellow);
 			process.exit(1);
 		});
@@ -34,13 +34,15 @@ function getAccessToken(oa, oauth_token, oauth_token_secret, pin) {
 function getRequestToken(oa) {
 
 	oa.getOAuthRequestToken(function(error, oauth_token, oauth_token_secret, results){
-		if(error) {
-			throw new Error(([error.statusCode, error.data].join(': ')).bold.red);
+		if (error) {
+			throw new Error(([ error.statusCode, error.data ].join(': ')).bold.red);
 		} else {
-			console.log('In your browser, log in to your twitter account.  Then visit:'.bold.green)
-			console.log(('https://twitter.com/oauth/authorize?oauth_token=' + oauth_token).underline.green)
-			console.log('After logged in, you will be promoted with a pin number'.bold.green)
-			console.log('Enter the pin number here:'.bold.yellow);
+			console.log('In your browser, log in to your twitter account.  Then visit:');
+			console.log(('https://twitter.com/oauth/authorize?oauth_token=' + oauth_token).underline.cyan);
+			console.log('After logged in, you will be promoted with a pin number.');
+			console.log('');
+			console.log('Enter the pin number here:'.bold.green);
+
 			var stdin = process.openStdin();
 			stdin.on('data', function(chunk) {
 				pin = chunk.toString().trim();
